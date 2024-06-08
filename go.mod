27d5c1ffe599e62efc07b43a2e54dc2e0016834d
Michael Thornes <michael@zyte.me>
2024-06-08 16:10:35 -0700

    add config path cli argument

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22
 require github.com/gomarkdown/markdown v0.0.0-20240419095408-642f0ee99ae2
 
 require (
+	github.com/spf13/pflag v1.0.5
 	github.com/zyedidia/generic v1.1.0
 	github.com/zytekaron/gorl v0.1.0
 	gopkg.in/yaml.v3 v3.0.1
--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"fs/src/cache"
 	"fs/src/config"
 	"fs/src/server"
+	"github.com/spf13/pflag"
 	"log"
 )
 
+var cfgPath string
 var cfg *config.Config
 
 func init() {
+	pflag.StringVarP(&cfgPath, "config", "c", "./config.yml", "The path to the config file")
+	pflag.Parse()
+
 	var err error
-	cfg, err = config.Load("config.yml")
+	cfg, err = config.Load(cfgPath)
 	if err != nil {
 		log.Fatalln("error loading config.yml:", err)
 	}
